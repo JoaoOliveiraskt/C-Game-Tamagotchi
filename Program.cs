027e764296f0d91bcca827f60496ddf57f85c3d4
João Victor Oliveira <joaoliveiraa7@gmail.com>
2024-02-02 00:20:38 -0300

    integrates the pokemon api

---
+++ Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using RestSharp;
+using RestSharp.Authenticators;
+
+namespace MyGame
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            var client = new RestClient($"https://pokeapi.co/api/v2/pokemon/");
+            var request = new RestRequest("", Method.Get);
+            var response = client.Execute(request);
+
+            if(response.StatusCode == System.Net.HttpStatusCode.OK) {
+                Console.WriteLine(response.Content);
+            } else {
+                Console.WriteLine("Error:" + response.ErrorMessage);
+            }
+        }
+    }
+}
+
